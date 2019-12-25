0c8205448aec350007c331730e827ab4f492037a
Bas Buller <bas.buller@gmail.com>
2019-12-25 08:54:48 +0100

    change name and init python

--- init.el
+++ init.el
@@ -140,7 +140,10 @@
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
-       ;;python            ; beautiful is better than ugly
+       (python            ; beautiful is better than ugly
+        +conda
+        +pyenv
+        +lsp)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;rest              ; Emacs as a REST client
